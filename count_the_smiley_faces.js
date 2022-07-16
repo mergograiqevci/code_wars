06ed047f58ea52adfa429bdef5a186d3c3f75b46
Mergim Graiqevci <59068563+mergograiqevci@users.noreply.github.com>
2022-07-16 14:53:27 +0200

    count_the_smiley_faces

---
+++ count_the_smiley_faces.js
@@ -0,0 +1,47 @@
+// https://www.codewars.com/kata/583203e6eb35d7980400002a/train/javascript
+
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+const eyesAreValid = (s) => {
+    return s === ":" || s === ";";
+}
+
+const monthIsValid = (s) => {
+    return s === ")" || s === "D";
+}
+
+const countSmileys = (arr) => {
+    let smiley = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const face = arr[i];
+        let eyesValid = false;
+        let monthValid = false;
+        for (let j = 0; j < face.length; j++) {
+            if (eyesAreValid(face[j])) {
+                eyesValid = true;
+            }
+            if (monthIsValid(face[j])) {
+                monthValid = true;
+            }
+        }
+        if (eyesValid && monthValid) {
+            smiley++;
+        }
+    }
+    return smiley;
+}
+
+console.log(countSmileys([]) === 0);
+console.log(countSmileys([':D', ':~)', ';~D', ':)']) === 4);
+console.log(countSmileys([':)', ':(', ':D', ':O', ':;']) === 2);
+console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']) === 1);
